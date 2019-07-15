fb42b0244869f7638530c243b169d7995405f753
Mariacruz4 <eloisa943@gmail.com>
2019-07-15 09:41:23 -0500

    replaced name with sum

--- ProDev/src/main/java/Name.java
+++ ProDev/src/main/java/Name.java
@@ -1,6 +1,9 @@
 public class Name {
     public static void main(String[] args) {
 
-        System.out.println("Maria");
+        int x = 4;
+        int y = 5;
+
+        System.out.println(x + y);
     }
 }
